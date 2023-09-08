0aa0ba97ba47ac0c5de06fa3ab30029065c0b646
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-09-08 13:17:20 +0100

    loaded register function in AuthController and also load laravel sanctum for authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function user(){
         return 'authenticated user';
+    }
+
+    public function register(Request $request){
+       $user = User::create([
+        'name' => $request->input('name'),
+        'email' => $request->input('email'),
+        'password' => Hash::make($request->input('password'))
+       ]);
+
+       return response()->json($user);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('user', [AuthController::class, 'user']);
+Route::post('register', [AuthController::class, 'register']);
